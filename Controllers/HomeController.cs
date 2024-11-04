2d9feace330013746f248c8892b3be9873db9164
ottotirronen <otto.tirronen@edu.karelia.fi>
2024-11-04 23:12:11 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using TuoteDemo.Models;
+
+namespace TuoteDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Product()
+
+        {
+
+            var products = GetProducts();
+
+            return View(products);
+
+        }
+
+        private List<Product> GetProducts()
+
+        {
+
+            var json = System.IO.File.ReadAllText("wwwroot/products.json");
+
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelectorAll('.product-card h2').forEach(function (element) {
+
+        element.textContent = element.textContent.replace(/\b\w/g, function (char) {
+
+            return char.toUpperCase();
+
+        });
+
+    });
+
+
+
+    document.querySelectorAll('.product-card p:nth-child(4)').forEach(function (element) {
+
+        let price = parseFloat(element.textContent.replace('Price: ', '').replace(' €', ''));
+
+        element.textContent = `Price: ${price.toFixed(2).replace('.', ',')} €`;
+
+    });
+
+});
